56037970ad56086a937bc80289f727d6cced1c9e
Lawe Zangena <lawza02@gmail.com>
2021-11-12 15:24:10 +0100

    added all the files

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var home_controller = require('../controller/HomeController');
+var home_controller = require('../controller/homeController');
 
 router.get('/', home_controller.index);
 
